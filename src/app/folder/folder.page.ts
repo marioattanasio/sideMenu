c07a150bb7ace819893de169f087c4a4f54991ff
unknown <mario.attanasio@acomeapp.com>
2020-05-06 09:34:49 +0200

    First commit

---
+++ src/app/folder/folder.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import {  MenuController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-folder',
+  templateUrl: './folder.page.html',
+  styleUrls: ['./folder.page.scss'],
+})
+export class FolderPage implements OnInit {
+  public folder: string;
+
+  constructor(private activatedRoute: ActivatedRoute,public menu: MenuController) { }
+
+  ngOnInit() {
+    this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+  }
+
+  ionViewWillEnter() {
+    this.menu.enable(true);
+   }
+
+}
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+import {  MenuController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor(public menu: MenuController,private router: Router) {}
+
+  ngOnInit() {
+  }
+
+
+  ionViewWillEnter() {
+    this.menu.enable(false);
+   }
+
+   goAnOtherPage(){
+ this.router.navigate(['/folder/Inbox']);
+
+   }
+
+
+}
